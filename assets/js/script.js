4ca89da426f9758a04b9f979ca76af07ef873303
Cayean <collinayala@gmail.com>
2022-08-06 11:46:26 -0500

    removed console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ var createTaskHandler = function() {
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
-
-    console.log(event)
 };
 
 formE1.addEventListener("submit", createTaskHandler);
